3078a5c31f3c0addeae7b97830000ffd1e17e416
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2020-11-12 10:44:29 +0100

    Add files via upload

---
+++ kostroEugenDrugiZadatak12112020/kostroEugenDrugiZadatak12112020/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenDrugiZadatak12112020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite niz znakova: ");
+            string a = Console.ReadLine();
+            a = a.Replace(" ", "_");
+            Console.WriteLine(a);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
